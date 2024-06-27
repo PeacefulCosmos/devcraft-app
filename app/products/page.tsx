961a5c0d1416e75f11569c03e04c777195c87a8f
roychan <cosmosroy0207@gmail.com>
2024-06-27 19:29:59 +0800

    test

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,19 @@
+import Link from "next/link";
+
 export default function ProductList() {
   return (
     <>
+      <Link href="/">Home</Link>
       <h1>Products List</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <Link href="/products/1">
+        <h2>Product 1</h2>
+      </Link>
+      <Link href="/products/2">
+        <h2>Product 2</h2>
+      </Link>
+      <Link href="/products/3" replace>
+        <h2>Product 3</h2>
+      </Link>
     </>
   );
 }
